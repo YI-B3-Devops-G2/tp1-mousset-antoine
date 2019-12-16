aa9895b6f2a92f9236adb819ff277efbdc39465d
Antoine Mousset <56311233+nrgix2@users.noreply.github.com>
2019-12-16 17:29:28 +0100

    Add files via upload

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,15 @@
+sudo apt-get update
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install -y nginx
 sudo apt install -y nodejs
+sudo systemctl start nginx
+     cd /var/www/html
+     sudo echo "<!DOCTYPE html>
+     <html>
+     <title> test </title>
+     <body>
+     B3 Devops - TP 1
+     </body>
+     </html>" > index.nginx-debian.html
+     sudo systemctl restart nginx
